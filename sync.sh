454d5faa62d9f377153d204cb752626cda190faa
Jerry <jfu@t-sciences.com>
2015-11-02 19:22:18 -0800

    Fix .apitokens spelling

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude ".apitoken" -av . ~
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude ".apitokens" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
