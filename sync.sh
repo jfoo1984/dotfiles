09e14a45f71a5469e413fbbe9890a4da0442b591
Jerry Fu <jerry.fu@gmail.com>
2020-04-09 18:36:41 -0700

    optimizations and clarity in sync script

--- sync.sh
+++ sync.sh
@@ -1,17 +1,65 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 git pull
-function doIt() {
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude "shell_scripts/apitokens.sh" --exclude "homebrew/" --exclude "Brewfile" --exclude "itermColors/" --exclude "Sublime/" -av . ~
+
+excluded_dirs=(".git/" ".DS_Store" "sync.sh" "README.md" "shell_scripts/apitokens.sh" "homebrew/" "Brewfile" "itermColors/")
+
+# build base rsync command with arguments
+rsync_command_args=(rsync)
+
+# add excluded dirs in command
+for excluded_dir in "${excluded_dirs[@]}"
+do
+  rsync_command_args+=(--exclude)
+  rsync_command_args+=($excluded_dir)
+done
+
+rsync_command_args+=(-av)
+
+if [ "$dry_run" = true ] ; then
+  rsync_command_args+=(-n)
+fi
+
+function dry_run_dot_files_rsync() {
+  dry_run_rsync_command=("${rsync_command_args[@]}")
+  dry_run_rsync_command+=(-n)
+
+  # sync source
+  dry_run_rsync_command+=(.)
+  # sync dest
+  dry_run_rsync_command+=(~)
+
+  "${dry_run_rsync_command[@]}"
 }
+
+function copy_dot_files_to_home_dir() {
+  copy_rsync_command=("${rsync_command_args[@]}")
+
+  # sync source
+  copy_rsync_command+=(.)
+  # sync dest
+  copy_rsync_command+=(~)
+
+  "${copy_rsync_command[@]}"
+  source ~/.bash_profile
+}
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
+  copy_dot_files_to_home_dir
 else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-  echo
+  # dry run to generate file list
+  echo -e "\n=== Generating list of files to update ==="
+  dry_run_dot_files_rsync
+
+  echo -e "\nWARNING: The files listed above will be overwritten in your home directory."
+  read -p "Are you sure you want to continue? (y/n) " -n 1
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
+    dry_run=false
+    echo "\n"
+    copy_dot_files_to_home_dir
   fi
 fi
-unset doIt
-source ~/.bash_profile
+
+echo
+unset dry_run_dot_files_rsync
+unset copy_dot_files_to_home_dir
